0081880f1c96b123df039538f934bcb05d6b1728
eevaain <evanlin416@gmail.com>
2022-07-22 15:34:42 -0400

    poggers

--- OpenGLTest3/game.cpp
+++ OpenGLTest3/game.cpp
@@ -13,15 +13,35 @@ void initGrid(int x, int y)
 void unit(int, int); //wait why are we putting definition here?
 void drawGrid()
 {
-	unit(20, 20);
+	// make a for loop to make the grid out of the small units
+	for (int x = 0; x < gridX; x++)
+	{
+		for (int y = 0; y < gridY; y++)
+		{
+			unit(x, y);
+		}
+	}
 }
 
 // defining dimensions of a single unit
 void unit(int x, int y)
 {   // between glBegin and glEnd we draw all our stuff here
-
-	glLineWidth(1.0);
-	glColor3f(1.0, 1.0, 1.0);
+	
+	// if x coord is 0 or y coord is 0 or x coord is at end of screen
+	// or y coord is at end of screen
+	if (x == 0 || y == 0 || x == gridX - 1 || y == gridY - 1)
+	{
+		glLineWidth(3.0);
+		glColor3f(1.0, 0.0, 0.0);
+	}
+	else
+	{
+		//we add back the old code since we turned this entire block into an
+		// if else condition so we can create different color block to represent
+		// border blocks
+		glLineWidth(1.0);
+		glColor3f(1.0, 1.0, 1.0);
+	}
 
 	glBegin(GL_LINE_LOOP); // this argument sees all the code from
 	// glBegin to glEnd and connects the coordinate points into a square
--- OpenGLTest3/main.cpp
+++ OpenGLTest3/main.cpp
@@ -1,22 +1,28 @@
 // REMEMBER FILE HAS TO BE CALLED MAIN TO LINK OTHER SHITS
 #include "game.h"
-#include <GL/freeglut.h> // HOLY SHIT I FIXED IT OK SO I NEEDED THIS
+#include <GL/freeglut.h> // HOLY SHIT I FIXED IT OK SO I NEEDED THIS!!!
 #include <GL/glut.h>
+
 
 
 //make some constants 
 #define COLUMNS 40
 #define ROWS 40
 
+//define constant for FPS
+#define FPS 10
+
 // we write this function definition so that we can write its body
 // at the bottom??? it works when i have the function body at the top. 
 // remember it only runs inside of main()
+
+void timer_callback(int);
 void display_callback();
 void reshape_callback(int, int);
 
 void init()
 {
-	glClearColor(0.0, .0,1.0, 1.0);
+	glClearColor(0.0, 0.0,0.0, 1.0);
 
 	// we initialize our grid right here
 	initGrid(COLUMNS, ROWS);
@@ -32,6 +38,12 @@ int main(int argc, char** argv) // ???
 	glutCreateWindow("SNAKE"); // creates window
 	glutDisplayFunc(display_callback); //passing func in func
 	glutReshapeFunc(reshape_callback);
+
+
+	glutTimerFunc(0, timer_callback, 0);
+
+
+
 	// callbacks are passing functions as arguments to another func?
 	init(); // this thing allows us to plot our shit on the window?
 	glutMainLoop();// keeps our program running in a loop 
@@ -40,16 +52,34 @@ int main(int argc, char** argv) // ???
 	return 0;
 }
 // this is our callback function 
+
+int index = 0; // keep track of position of cube
 void display_callback()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	drawGrid();
+	// draws a rectangle using the two points given (so its 2x2)
+	// same thing as glVertex x4
+	//x1 = x coord of rectangle vertex
+	// x2 = opp vertex coord
+	// ^^ with x,y twice, defining 2 points
+	glRectd(index, 20, index + 2, 22); // y is gonna stay the same
+	// the function is only moving across x
+	index++; 
+	if (index > 40)
+	{
+		index = 0;
+	}
 	glutSwapBuffers();//puts the thing on the screen using double buffering
 	// so when one frame is processing onto the screen, another frame is 
 	// already put on. goal: always have a frame present at the front. 
 	// we dont want a loading frame at the front
 	// so imagine a shakespearean play 
 	// WE WANT THERE TO BE NO GAP BETWEEN SCENES
+	// tiny delay is added by glutSwapBuffers so frame rate is not exactly 10
+	
+	//at end of display callback there will be a new frame -> this is baseline
+	// for FPS
 }
 
 void reshape_callback(int w, int h)
@@ -63,4 +93,11 @@ void reshape_callback(int w, int h)
 	// 1 behind screen -> we plot our pixels on 0 for z so it is visible
 	glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
+}
+
+// new frame will be displayed every time this function is called
+void timer_callback(int)
+{
+	glutPostRedisplay();
+	glutTimerFunc(1000 / FPS, timer_callback, 0);
 }
